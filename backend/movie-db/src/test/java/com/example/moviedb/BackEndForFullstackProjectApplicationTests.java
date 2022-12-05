9d0c018e2e78ed1dec9c367b9a3806ac1cde1056
Zain-Ashfaq <zashfaq00@gmail.com>
2022-12-05 16:36:22 +0000

    test git

---
+++ backend/movie-db/src/test/java/com/example/moviedb/BackEndForFullstackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndForFullstackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
