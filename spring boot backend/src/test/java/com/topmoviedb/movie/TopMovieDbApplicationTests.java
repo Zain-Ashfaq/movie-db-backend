0a7dbe9e8414ba06f398d71ee6da05dd2e3244a7
Zain-Ashfaq <zashfaq00@gmail.com>
2022-12-05 15:36:09 +0000

    another commit

---
+++ spring boot backend/src/test/java/com/topmoviedb/movie/TopMovieDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topmoviedb.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopMovieDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
